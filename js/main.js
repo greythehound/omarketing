e8734067c978fd38c36c935e613c6163aff74f6f
Sergey Kizenko <sergey.kizenko@walkme.com>
2020-11-04 23:54:29 +0200

    adding adaptive header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+
+  const menuButton = document.querySelector(".menu__btn");
+  const headerTopInner = document.querySelector(".header__top-inner");
+
+
+  menuButton.addEventListener('click', function () {
+    headerTopInner.classList.toggle('header__top-inner--active');
+  });
+
+});
+
 $(function () {
   var mixer = mixitup('.portfolio__content');
   
@@ -5,7 +17,6 @@ $(function () {
     dots: true,
     prevArrow: `<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="arrow left"></button>`,
     nextArrow: `<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="arrow right"></button>`,
-    });
-
-
+  });
+  
 });
